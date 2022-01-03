cc2fb908ade0a24adccd48db7db86e2f0e9ff338
Sina <bekarsina@gmail.com>
2022-01-03 21:43:01 +0330

    commit part #12

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,61 @@
 document.addEventListener("DOMContentLoaded", function () {
   var elems = document.querySelectorAll(".sidenav");
-  var instances = M.Sidenav.init(elems, { edge: "right" });
+  var instances = M.Sidenav.init(elems, {
+    edge: "right",
+  });
 
-  var tabs = document.querySelectorAll(".main-ul-navbar");
-  M.Tabs.init(tabs, { swipeable: true });
+  var tabs = document.querySelectorAll(".main_ul_navbar");
+  M.Tabs.init(tabs, {
+    swipeable: true,
+  });
 
   var actionButton = document.querySelectorAll(".fixed-action-btn");
-  var instances = M.FloatingActionButton.init(actionButton, { toolbarEnabled: true});
+  M.FloatingActionButton.init(actionButton, {
+    toolbarEnabled: true,
+  });
+
+  var selectElem = document.querySelectorAll("select");
+  M.FormSelect.init(selectElem, {});
+
+  var quickAccessNodes = Array.prototype.slice.call(
+    document.querySelectorAll(".quick_access_items")
+  );
+
+  for (const item of quickAccessNodes) {
+    item.addEventListener("click", function () {
+      let index = quickAccessNodes.indexOf(item) + 1;
+      let menuItemArray = document.querySelectorAll(".menu_item_list");
+      setTimeout(function () {
+        menuItemArray[index].click();
+      }, 100);
+    });
+  }
+  animateProgress("payProgressPath", "payProgressText", 50);
+  animateProgress("floorProgressPath", "floorProgressText", 90);
+  function animateProgress(PathDomId, textDomId, percent) {
+    let startPercent = 0;
+    let maxPercent = percent;
+
+    let startProgress = 0;
+    let maxProgress = 253;
+
+    let progressInterval = setInterval(function () {
+      if (startPercent <= maxPercent) {
+        document
+          .getElementById(PathDomId)
+          .setAttribute(
+            "stroke-dasharray",
+            `${startProgress} , ${maxProgress}`
+          );
+
+        document.getElementById(textDomId).innerHTML = startPercent + "%";
+
+        startPercent += 1;
+        startProgress += 2.53;
+        maxProgress -= 2.53;
+      } else {
+        clearInterval(progressInterval);
+      }
+    }, 25);
+  }
 });
